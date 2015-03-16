666b6b21944f8e6a2bea80f38006612be0108e56
Arjun B <arjunb090@gmail.com>
2015-03-16 14:00:26 -0700

    Inserted function for handling arrays

--- combn.cu
+++ combn.cu
@@ -1,41 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
+#include <time.h>
+#include <omp.h>
 using namespace std;
 
+//Prints a singe vector out
+void print1d(const vector<int>& v) {
+  static int count = 0;
+  for(int i = 0; i < v.size(); i++) {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+};
 
+//prints a 2D vector out 
+void print2d(const vector<vector<int> > &v) {
+  for(int i = 0; i < v.size(); i++) {
+    for(int j = 0; j < v[i].size(); j++) {
+      cout << v[i][j] << " ";
+    }
+    cout << endl;
+  }
+};
 
+vector< vector<int> > combn(int n, int r) {
+  vector<bool> v(n);
+  fill(v.begin() + r, v.end(), true);
+  vector < vector<int> > allCombinations;
+  omp_set_num_threads(8);
 
-int * combn(int x, int m) {
-	int *ret = new int[x * m];
+  do {
+    vector<int> combinations;
+    #pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+      if (!v[i]) {
+        #pragma omp critical
+        combinations.push_back((i+1));
+       }
+    }
+    sort(combinations.begin(), combinations.end());
+    allCombinations.push_back(combinations);
+   } while (std::next_permutation(v.begin(), v.end()));
 
-	int loc = 0;
-	for(int i = 0; i < x; i++) {
-		for(int j = i; j < i + m; j++) {
-			ret[loc] = j;
-			loc++;
-		}
-	}
-	return ret;
+   return allCombinations;
 }
-
-
-template <typename T> 
-T * combn(T x, int m) {
-
-	
-}
-
 
 int main (int argc, char** argv) {
-
-	int x = 10;
-	int m = 2;
-	int * vals[x * m];
-    vals = combn(x,m);
-    for (int i = 0; i < x * m; i++) {
-    	cout << vals[x];
-    }
-    return 0;
+  int x = 5;
+  int m = 3;
+  vector<vector<int> > vals;
+  // clock_t startTime = clock();
+  vals = combn(x,m);
+  // cout << double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." << endl;
+  // cout << "Total combinations: " << vals.size() << endl;
+  // print2d(vals);
+  return 0;
 }
