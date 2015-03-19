30076b6b11d1f9aa4bbe03d891f7308cc1cf4733
Bijan S Agahi <bagahi@pc44.cs.ucdavis.edu>
2015-03-19 03:49:02 -0700

    Incomplete. V0.0.1

---
+++ combn_bij.R
@@ -0,0 +1,20 @@
+library("parallel")
+library("snow")
+
+combn_R <- function(x,m,FUN=NULL,simplify=0){
+  size <- choose(x,m)
+  values <- seq_len(x)
+  allCombs <- c() #initialize allCombs
+  numEntriesPerLevel <- function(x,m){
+    array[0] <- 0
+    for(i in 1:(x-m+1)){
+      array[i] = array[i-1]+(choose(x-i,m-1)*m)
+    }
+    array
+  }
+  print(numEntriesPerLevel)
+  combination <- c() #initialize combination
+  for(i in 1:(x-m+1)){
+    combination <- append(combination,i)
+  }
+}
