c7644e03e53a670f68ff2604d7eb513386ea10e4
arjunbharadwaj94 <abharadwaj@ucdavis.edu>
2015-03-19 01:33:56 -0700

    Updated stefan's code

--- combn.cpp
+++ combn.cpp
@@ -1,30 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <time.h>
-#include <omp.h>
+#include <algorithm>    // std::find
+
 using namespace std;
 
-vector < vector<int> > allCombinations;
+vector<int> values;
+//vector<int> combination;
+vector<vector <int> > allCombinations;
+int *allCombs;
+int *numEntriesPerLevel;
 
-int iter_factorial(int n)
-{
-  int ret = 1;
-  for(unsigned int i = 1; i <= n; ++i)
-    ret *= i;
-  return ret;
+double choose(int n, int k) {
+    if(k == 0) return 1; 
+    return (n * choose(n - 1, k - 1)) / k;
 }
 
-//Prints a singe vector out
 void print1d(const vector<int>& v) {
   static int count = 0;
   for(int i = 0; i < v.size(); i++) {
     cout << v[i] << " ";
   }
   cout << endl;
-};
+}
 
-//prints a 2D vector out 
 void print2d(const vector<vector<int> > &v) {
   for(int i = 0; i < v.size(); i++) {
     for(int j = 0; j < v[i].size(); j++) {
@@ -32,90 +32,87 @@ void print2d(const vector<vector<int> > &v) {
     }
     cout << endl;
   }
-};
-
-void combn(vector<bool> permutation, int index) {
-  vector<int> calculatedCombo;
-  // Determine the number of 0's and the positions
-  for(int i = 0; i < permutation.size(); i++) {
-    if(!permutation[i]) {
-      calculatedCombo.push_back(i+1);
-    }
-  }
-
-  allCombinations[index] = calculatedCombo;
 }
 
-void calculateCombinations(int n, int r) {
-  // Calculate total combinations and allocate well in advance
-  allCombinations.resize( iter_factorial(n) / (iter_factorial(r) * iter_factorial(n - r)) );
 
-  // Local Permutation storer
-  vector< vector<bool> > allPermutations;
-  omp_set_num_threads(8);
-  vector<bool> v(n);
-  fill(v.begin() + r, v.end(), true);
 
-  // Push back all the permutations of 1's and 0's
-  do {
-    allPermutations.push_back(v);
-  } while(std::next_permutation(v.begin(), v.end()));
+void addComb(vector<vector<int> > &v, vector<int> &v2) {
+  v.push_back(v2);
+}
 
-  int s = allPermutations.size();
+void findCombs(int offset, int k, vector<int> &combination, vector<vector <int> > &v) {
+  if (k == 0) {
+    //#pragma omp critical
+    //print1d(combination);
+    #pragma omp critical
+    addComb(allCombinations, combination);
+    return;
+  }
+  for(int i = offset; i <= values.size() - k; ++i) {
+    combination.push_back(values[i]);
+    findCombs(i + 1, k - 1, combination, v);
+    combination.pop_back();
+  }
+}
+
+void findEntriesPerLevel(int *array, int x, int m) {
+  array[0] = 0;
+  for(int i = 1; i < x - m + 1; i++) {
+    array[i] = array[i] + (choose(x - i - 1, m - 1) * m);
+  }
+
+}
+
+void vectorToArray(vector<vector <int> > &v, int a[], int startIndex) {
+  vector< vector<int> >::const_iterator row; 
+    vector<int>::const_iterator col; 
+    int index = startIndex;
+
+    for (row = v.begin(); row != v.end(); ++row)
+    { 
+         for (col = row->begin(); col != row->end(); ++col)
+         { 
+            a[index] = *col; 
+            index++;
+         } 
+    } 
+}
+
+int * combn(int x, int m) {
+  int size = choose(x,m); 
+  for(int i = 0; i < x; ++i)
+    values.push_back(i+1);
+
+  allCombs = new int[size * m];
+  numEntriesPerLevel = new int[x - m + 1];
+  findEntriesPerLevel(numEntriesPerLevel, x, m);
+  //combination.reserve(5);
+
   #pragma omp parallel for
-  for(int i = 0; i < s; ++i) {
-    combn(allPermutations[i], i);
+  for(int i = 1; i <= x - m + 1; i++){  
+    vector<vector <int> > levelCombinations;
+    vector<int> combination;
+    combination.push_back(i);
+    findCombs(i, m - 1, combination, levelCombinations);
+    // vectorToArray(levelCombinations, allCombs, numEntriesPerLevel[i - 1]);
+    combination.pop_back();
   }
+  //#pragma omp barrier
+  //print2d(allCombinations);
 
+  return allCombs;
 }
 
-vector< vector<int> > combnArr(vector<int> arr, int r) {
-  int n = arr.size();
-  vector<bool> v(n);
-  //sort(arr.begin(), arr.end());
-  fill(v.begin() + r, v.end(), true);
-  
-  omp_set_num_threads(8);
 
-  do {
-    vector<int> combinations;
-    #pragma omp parallel for
-    for (int i = 0; i < n; ++i) {
-      cout << v[i];
-      if (!v[i]) {
-        #pragma omp critical
-        combinations.push_back(arr[i]);
-       }
-       cout << endl;
-    }
-    sort(combinations.begin(), combinations.end());
-    allCombinations.push_back(combinations);
-   } while (std::next_permutation(v.begin(), v.end()));
-
-  return allCombinations;
-}
 
 int main (int argc, char** argv) {
-  int x = 5;
+
+  int x = 8;
   int m = 3;
-  // vector<int> arr;
-  // arr.push_back(5);
-  // arr.push_back(100);
-  // arr.push_back(88);
-  // arr.push_back(2);
-  // arr.push_back(1);
-  // arr.push_back(43);
-  // arr.push_back(3);
-  // arr.push_back(9);
-  // arr.push_back(0);
-  // arr.push_back(820);
-  // vector<vector<int> > vals(10);
-  clock_t startTime = clock();
-  calculateCombinations(x,m);
-  double z =  double( clock() - startTime ) / (double)CLOCKS_PER_SEC;
-  print2d(allCombinations);
-  cout << "Total combinations: " << allCombinations.size() << endl;
-  cout << "Total seconds = " << z << endl;
-  //print2d(allCombinations);
-  return 0;
+  int * vals;
+    vals = combn(x,m);
+    int count = 0;
+    // /print2d(vals);
+    print2d(allCombinations);
+    return 0;
 }
