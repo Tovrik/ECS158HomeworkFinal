923601246c000c506dc2a15c5f1ca5c954359c4d
bijanagahi <bijanagahi@users.noreply.github.com>
2015-03-19 19:47:35 -0700

    Contains Errors in findCombs function. V0.0.3

--- combn_bij.R
+++ combn_bij.R
@@ -1,20 +1,46 @@
 library("parallel")
 library("snow")
 
+#cls <- parallel::makePSOCKcluster(rep('localhost',4))
+
 combn_R <- function(x,m,FUN=NULL,simplify=0){
   size <- choose(x,m)
   values <- seq_len(x)
   allCombs <- c() #initialize allCombs
-  numEntriesPerLevel <- function(x,m){
-    array[0] <- 0
-    for(i in 1:(x-m+1)){
-      array[i] = array[i-1]+(choose(x-i,m-1)*m)
+  allCombinations <- list()
+  findEntriesPerLevel <- function(x,m){
+    array <- array(rep(0,(x-m+1)))
+    array[1] <- 0
+    for(i in 2:(x-m+1)){
+      array[i] <- array[i-1]+(choose(x-i+1,m-1)*m)
+      #print(array[i])
     }
     array
   }
+  addComb <- function(v, v2){
+    v[[length(v)+1]] <- v2
+  }
+  findCombs <- function(offset, k, v, combination){
+    if(k==0){
+      addComb(v, combination)
+      0
+    }
+    for(i in offset:length(values)-k){
+      combination[length(combination)+1] <- values[i]
+      findCombs(i+1,k-1,v,combination)
+      combination <- combination[-(length(combination))]
+    }
+  }
+  numEntriesPerLevel <- findEntriesPerLevel(x,m)
+  print(numEntriesPerLevel)
   #print(numEntriesPerLevel)
   combination <- c() #initialize combination
   for(i in 1:(x-m+1)){
-    combination <- append(combination,i)
+    levelCombinations <- list()
+    combination[length(combination)+1] <- i
+    findCombs(i, m-1, levelCombinations, combination)
+    combination <- combination[-(length(combination))]
   }
 }
+
+combn_R(5,3)
