f17e28487698b28c872c701ea1b5ebd5b7030303
Stefan Peterson <stefan.t.peterson@gmail.com>
2015-03-20 14:35:40 -0700

    working on the writeup

--- combnOpenMP.cpp
+++ combnOpenMP.cpp
@@ -8,8 +8,6 @@
 using namespace std;
 
 vector<int> values;
-// vector<int> combination;
-vector<vector <int> > allCombinations;
 int *allCombs;
 int *numEntriesPerLevel;
 
@@ -18,40 +16,12 @@ double choose(int n, int k) {
     return (n * choose(n - 1, k - 1)) / k;
 }
 
-void print1d(const vector<int>& v) {
-	static int count = 0;
-	for(int i = 0; i < v.size(); i++) {
-		cout << v[i] << " ";
-	}
-	cout << endl;
-}
-
-void print2d(const vector<vector<int> > &v) {
-	for(int i = 0; i < v.size(); i++) {
-		for(int j = 0; j < v[i].size(); j++) {
-			cout << v[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
-
-void printArray(int a[], int size, int m) {
-	for(int i = 0; i < size; i++){
-		if(i % m == 0)
-			cout << endl;
-		cout << a[i] << " ";
-	}
-	cout << endl;
-}
-
-
 void addComb(vector<vector<int> > &v, vector<int> &v2) {
 	v.push_back(v2);
 }
 
 void findCombs(int offset, int k, vector<vector <int> > &v, vector<int> combination) {
 	if (k == 0) {
-		// print1d(combination);
 		addComb(v, combination);
 		return;
 	}
@@ -102,33 +72,8 @@ int * combn(int x, int m) {
 		findCombs(i, m - 1, levelCombinations, combination);
 		vectorToArray(levelCombinations, allCombs, numEntriesPerLevel[i - 1]);
 		combination.pop_back();
-		// print2d(levelCombinations);
 	}
-	// printArray(allCombs, size * m, m);
 	#pragma omp barrier
 
 	return allCombs;
-}
-
-
-
-int main (int argc, char** argv) {
-
-	int x = 28;
-	int m = 14;
-	int * vals;
-	double startTime, endTime;
-	startTime = omp_get_wtime();
-    vals = combn(x,m);
-    endTime = omp_get_wtime();
-
-	printf("%f,",endTime-startTime);
-
-cout << choose(26,14);
-    int count = 0;
-
-    delete[] allCombs;
-    delete[] numEntriesPerLevel;
-
-    return 0;
 }
